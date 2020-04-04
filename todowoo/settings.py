c8f5fa117c41581730126d526f2393f665395dcf
Amit Singh <62190590+playground-debug@users.noreply.github.com>
2020-04-05 01:06:28 +0530

    Update settings.py

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y4u_+&dns0_!1lk6lgb#*0+j+)*&bf13o#*7svk+fq*(e+3moq'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
